16535e921101ac5c7bb73ba5cf8cf9ec55e6fbaa
etu5 <massie@irit.fr>
2013-02-24 00:11:06 +0100

    1er commit

---
+++ MySimpleXML/src/main/java/dcll/hmas/MySimpleXML/App.java
@@ -0,0 +1,13 @@
+package dcll.hmas.MySimpleXML;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
