f88a9aae40ddb3f06b4279f5aab6de23562aed17
HenriMassie <massie@irit.fr>
2013-02-25 01:03:51 +0100

    5éme commit : message complété et essai push via terminal

--- MySimpleXML/src/main/java/dcll/hmas/MySimpleXML/App.java
+++ MySimpleXML/src/main/java/dcll/hmas/MySimpleXML/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Salut tout le monde!" );
+        System.out.println( "Salut tout le monde! Comment ca va ?" );
     }
 }
