8e84998c2824f51a412028e7c9ebdfde1e5ef253
HenriMassie <massie@irit.fr>
2013-02-26 10:58:02 +0100

    #1 modif message et expression en francais

--- MySimpleXML/src/main/java/dcll/hmas/MySimpleXML/App.java
+++ MySimpleXML/src/main/java/dcll/hmas/MySimpleXML/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Git, Github et Maven, c'est top" );
+        System.out.println( "Git, Github et Maven, c'est trop top" );
     }
 }
